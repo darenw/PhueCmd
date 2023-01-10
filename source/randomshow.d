0ec4b37aec94378f518167440e773b89566ce75a
darenw <darenw@darenscotwilson.com>
2023-01-09 21:20:27 -0700

    Now has functional random show

--- source/randomshow.d
+++ source/randomshow.d
@@ -1,6 +1,7 @@
 // Run lights randomly
 
 import std.stdio;
+import core.stdc.stdlib;
 import std.format;
 import std.string;
 import dlangui;
@@ -8,11 +9,48 @@ import core.thread;
 import phuecolor;
 import phuesystem;
 
+//extern (C) int kbhit();
+//extern (C) int getch();
 
+
+// Settings - hardcoded for now. In future, will be GUI sliders, checkboxes etc
+
+float max_brightness = 0.5f;
+Duration pause = dur!("seconds")(2);    // fast-paced for testing
+int nsteps = 5; //11;
 
 
 void run_random_show(PhueSystem system)  {
     
-    // Just a crude sanity check: prove we're here by making bright yellow
-    system.bulbs[0].set( PhueColor(0.98, 0.53, 0.45) );
+    writeln("Tap ctrl-C or 'Q' to quit");
+    ulong nbulbs = system.bulbs.length;
+    
+    PhueColor[] color1;   color1.length = nbulbs; 
+    PhueColor[] color2;   color2.length = nbulbs;
+
+    foreach (i, b; system.bulbs)  {
+        b.set( ZERO_COLOR );
+        color1[i] = ZERO_COLOR;
+        color2[i] = random_color(0.5*max_brightness, max_brightness);
+    }
+    
+    
+    bool running = true;
+    while (running) {
+        for (float f = 0.0f;  f < 1.001f; f += 1.0f/nsteps)  {
+            foreach (i, b; system.bulbs)  {
+                b.set( sine_mix(color1[i], f, color2[i]) );
+            }
+            Thread.sleep(pause);
+        }
+        
+        // Update color targets.
+        color1[] = color2;
+        foreach (i; 0 .. nbulbs)  {
+            color2[i] = random_color(0.2f*max_brightness, max_brightness);
+        }
+//        writeln("  ", color1);
+//        writeln("  ", color2);        
+    }
+    
 }
